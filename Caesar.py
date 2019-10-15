f8a19dcc04361a41c636ac6ab209ddec9149b0db
Can Yuva <canyuva06@gmail.com>
2019-10-15 20:37:42 +0300

    added caesar

---
+++ Caesar.py
@@ -0,0 +1,56 @@
+MAX_KEY = 29
+alphabet = "abcçdefgğhıijklmnoöprsştuüvyz"
+
+def getKey():
+    key = 0
+    while True:
+        print('Enter the key number (1-%s)' % (MAX_KEY))
+        key = int(input())
+        if (key >= 1 and key <= MAX_KEY):
+            return key
+
+def exists_digit(msg):
+    for i in msg:
+        if i.isdigit():
+            return True
+    return False
+
+def getMessage():
+    msg = input('Enter the message :')
+    if exists_digit(msg):
+        return 'err'
+    else:
+        return msg
+
+def encrypt(msg,key):
+    msg = str.lower(msg)
+    encrypted = ''
+    for letter in msg:
+        index = msg.index(letter)
+        encrypted+=alphabet[(index+int(key))%len(alphabet)]
+    return encrypted
+
+def decrypt(encrypted,key):
+    decrypted = ''
+    for letter in encrypted:
+        index = encrypted.index(letter)
+        decrypted+=alphabet[(index-key)%len(alphabet)]
+    return decrypted
+        
+def main():
+    print('163301015 - Bekir Can YUVA\nCaesar\n\nEncryption or Decryption ?\n1- Encryption\n2- Decryption')
+    selection = input('Selection : ')
+    if selection == '1':
+        msg = getMessage()
+        key = getKey()
+        encrypted = encrypt(msg,key)
+        print(encrypted)
+    elif selection == '2':
+        msg = getMessage()
+        key = getKey()
+        decrypted = decrypt(msg,key)
+        print(decrypted)
+    else:
+        print('Please enter a valid value')
+
+main()
